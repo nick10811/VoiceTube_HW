f9d745ea801b13c0eab0e9d799bbe1b2cec41112
Nick Yang <nick10811@gmail.com>
2018-05-14 18:47:21 +0800

    Create project

---
+++ VoiceTube_HW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceTube_HW
+//
+//  Created by Nick Yang on 2018/5/14.
+//  Copyright © 2018 Nick Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
