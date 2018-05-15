7e68a3001f72dfc3c8064f62ddb28f2802a3062d
Nick Yang <nick10811@gmail.com>
2018-05-15 09:58:09 +0800

    Add VideoCell

---
+++ VoiceTube_HW/View/VideoCell.swift
@@ -0,0 +1,26 @@
+//
+//  VideoCell.swift
+//  VoiceTube_HW
+//
+//  Created by Nick Yang on 2018/5/15.
+//  Copyright © 2018 Nick Yang. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell: UITableViewCell {
+    @IBOutlet weak var videoImageView: UIImageView!
+    @IBOutlet weak var videoTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
